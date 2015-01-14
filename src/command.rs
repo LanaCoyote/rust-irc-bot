493c7476ab660db2834b4298b3706a6c5037ffb8
David Reeve <nerdboy6@gmail.com>
2015-01-14 08:02:48 -0500

    Added support for raw and code commands
    Use command::register_raw_command to register a command that parses the
    raw message, or command::register_code_command for a command that parses
    non-privmsg messages
    
    This closes issue #1

--- src/command.rs
+++ src/command.rs
@@ -4,6 +4,26 @@ use rustirc::message;
 
 use bot;
 
+static WELCOME              : &str = "003";
+static ERR_NOSUCHNICK       : &str = "401";
+static ERR_NOSUCHSERVER     : &str = "402";
+static ERR_NOSUCHCHANNEL    : &str = "403";
+static ERR_CANNOTSENDTOCHAN : &str = "404";
+static ERR_TOOMANYCHANNELS  : &str = "405";
+static ERR_WASNOSUCHNICK    : &str = "406";
+static ERR_TOOMANYTARGETS   : &str = "407";
+static ERR_NOORIGIN         : &str = "409";
+static ERR_NORECIPIENT      : &str = "411";
+static ERR_NOTEXTTOSEND     : &str = "412";
+static ERR_NOTOPLEVEL       : &str = "413";
+static ERR_WILDTOPLEVEL     : &str = "414";
+
+pub enum Code {
+  Raw,
+  Privmsg,
+  Other ( _ ),
+}
+
 pub trait Cmd {
   fn on_cmd ( &mut self, msg : message::Message, cnt : &mut client::Client ) { }
 }
@@ -12,10 +32,11 @@ pub struct Command <'cl> {
   pattern : String,
   cb      : Box < Cmd + 'cl >,
   regex   : regex::Regex,
+  code    : Code,
 }
 
 impl <'cl> Command <'cl> {
-  pub fn new <'b> ( patt : &str, cb : Box < Cmd + 'b > ) -> Command <'b> {
+  pub fn new <'b> ( patt : &str, cb : Box < Cmd + 'b >, code : Code ) -> Command <'b> {
     let re = match regex::Regex::new ( patt ) {
       Ok ( re ) => re,
       Err ( e ) => panic! ( "error creating command regex : {}", e.msg ),
@@ -24,6 +45,7 @@ impl <'cl> Command <'cl> {
       pattern : patt.to_string( ),
       cb      : cb,
       regex   : re,
+      code    : code,
     }
   }
   
@@ -32,6 +54,20 @@ impl <'cl> Command <'cl> {
   }
   
   pub fn is_match ( &self, msg : message::Message ) -> bool {
-    self.regex.is_match( msg.trailing( ).expect( "No trailing on message" ) )
+    let cmpmsg = match self.code {
+      Raw             => msg.raw,
+      Privmsg         => {
+        match msg.code.as_slice( ) {
+          "PRIVMSG" | "NOTICE" => msg.trailing( ).expect_or( String::new( ) ),
+          _                    => return false,
+      },
+      Other ( code )  => {
+        match String::from_str( code ) {
+          msg.code => msg.trailing( ).expect_or( String::new( ) ),
+          _        => return false,
+        }
+      },
+    };
+    self.regex.is_match( cmpmsg )
   }
 }
--- src/events.rs
+++ src/events.rs
@@ -8,63 +8,41 @@ use callback;
 use command;
 
 pub struct EventDispatcher <'ed> {
-  msg_callbacks : Vec < &'ed (callback::IrcCallback + 'ed) >,
   cmd_callbacks : Vec < cell::RefCell < command::Command <'ed> > >,
 }
 
 impl <'ed> EventDispatcher <'ed> {
   pub fn new <'e> ( ) -> EventDispatcher <'e> {
     EventDispatcher {
-      msg_callbacks : Vec::new(),
       cmd_callbacks : Vec::new(),
     }
   }
   
   pub fn close ( &mut self ) {
-    self.msg_callbacks.clear( );
     self.cmd_callbacks.clear( );
   }
   
   pub fn handle_msg ( &mut self, msg : message::Message, cnt : &mut client::Client ) {
-    // OLD CALLBACK CODE, UNCOMMENT IF I REIMPLEMENT THEM
-    // for cb in self.msg_callbacks.iter( ) {
-      // cb.on_any( msg, bot );
-      // match msg.code.as_slice( ) {
-        // "INVITE"  => cb.on_invite( msg, bot ),
-        // "JOIN"    => cb.on_join( msg, bot ),
-        // "KICK"    => cb.on_kick( msg, bot ),
-        // "PRIVMSG" => {
-          // cb.on_msg( msg, bot );
-          // match msg.nick( ) {
-            // Some( s ) => {
-              // if s.as_slice( ).starts_with( "#" ) {
-                // cb.on_chanmsg( msg, bot );
-              // } else {
-                // cb.on_privmsg( msg, bot );
-              // }
-            // },
-            // None      => (),
-          // }
-        // },
-        // "NOTICE"  => cb.on_notice( msg, bot ),
-        // "PART"    => cb.on_part( msg, bot ),
-        // "003"     => cb.on_welcome( msg, bot ),
-        // _         => (),
-      // }
-    // }
-    
-    if msg.code.as_slice( ) == "PRIVMSG" {
-      for cmcell in self.cmd_callbacks.as_mut_slice( ).iter( ) {
-        let mut cmd = cmcell.borrow_mut( );
-        if cmd.is_match( msg.clone( ) ) {
-          cmd.call( msg.clone( ), cnt );
-        }
+    for cmcell in self.cmd_callbacks.as_mut_slice( ).iter( ) {
+      let mut cmd = cmcell.borrow_mut( );
+      if cmd.is_match( msg.clone( ) ) {
+        cmd.call( msg.clone( ), cnt );
       }
     }
   }
   
   pub fn register_command ( &mut self, patt : &str, cb : Box < command::Cmd + 'ed > ) {
-    let cmd = command::Command::new( patt, cb );
+    let cmd = command::Command::new( patt, cb, command::Code::Privmsg );
+    self.cmd_callbacks.push( cell::RefCell::new( cmd ) );
+  }
+  
+  pub fn register_raw_command ( &mut self, patt : &str, cb : Box < command::Cmd + 'ed > ) {
+    let cmd = command::Command::new( patt, cb, command::Code::Raw );
+    self.cmd_callbacks.push( cell::RefCell::new( cmd ) );
+  }
+  
+  pub fn register_code_command( &mut self, code : &str, patt : &str, cb : Box < command::Cmd + 'ed > ) {
+    let cmd = command::Command::new( patt, cb, command::Code::Other( code ));
     self.cmd_callbacks.push( cell::RefCell::new( cmd ) );
   }
 }
